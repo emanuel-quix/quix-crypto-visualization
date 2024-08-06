22073cc6204c39d010475d8e9010d21816e59ebd
Emanuel Ramos <emanuel@quix.io>
2024-08-06 13:08:51 +0000

    Edited 'main.py'

--- visualization-adapter/main.py
+++ visualization-adapter/main.py
@@ -23,7 +23,7 @@ def to_visualization_format(row):
 
     return {
         'symbol': symbol,
-        'timestamp': timestamp/1000,
+        'timestamp': timestamp * 1_000_000,
         'price': price,
         'datetime': datetime.fromtimestamp(timestamp / 1000).isoformat()
     }
